4a35ce66b45764cb8ad2a196a0ecfc051ef700c1
jingxiang <ruanxingbaozi@dingtalk.com>
2020-10-10 08:02:19 +0000

    init

---
+++ build/intermediates/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/AscendProjects/sample-objectdetection/src/dvpp_cropandpaste.cpp" "/home/user/AscendProjects/sample-objectdetection/build/intermediates/src/CMakeFiles/main.dir/dvpp_cropandpaste.cpp.o"
+  "/home/user/AscendProjects/sample-objectdetection/src/dvpp_jpegd.cpp" "/home/user/AscendProjects/sample-objectdetection/build/intermediates/src/CMakeFiles/main.dir/dvpp_jpegd.cpp.o"
+  "/home/user/AscendProjects/sample-objectdetection/src/dvpp_process.cpp" "/home/user/AscendProjects/sample-objectdetection/build/intermediates/src/CMakeFiles/main.dir/dvpp_process.cpp.o"
+  "/home/user/AscendProjects/sample-objectdetection/src/main.cpp" "/home/user/AscendProjects/sample-objectdetection/build/intermediates/src/CMakeFiles/main.dir/main.cpp.o"
+  "/home/user/AscendProjects/sample-objectdetection/src/model_process.cpp" "/home/user/AscendProjects/sample-objectdetection/build/intermediates/src/CMakeFiles/main.dir/model_process.cpp.o"
+  "/home/user/AscendProjects/sample-objectdetection/src/object_detect.cpp" "/home/user/AscendProjects/sample-objectdetection/build/intermediates/src/CMakeFiles/main.dir/object_detect.cpp.o"
+  "/home/user/AscendProjects/sample-objectdetection/src/utils.cpp" "/home/user/AscendProjects/sample-objectdetection/build/intermediates/src/CMakeFiles/main.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_DVPP_INTERFACE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/user/Ascend/ascend-toolkit/20.0.RC1/acllib_centos7.6.x86_64/acllib/include"
+  "../../src/../inc"
+  "/home/user/ascend_ddk/x86/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/intermediates/src/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "../../../out/main"
+  "../../../out/main.pdb"
+  "CMakeFiles/main.dir/dvpp_cropandpaste.cpp.o"
+  "CMakeFiles/main.dir/dvpp_jpegd.cpp.o"
+  "CMakeFiles/main.dir/dvpp_process.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/model_process.cpp.o"
+  "CMakeFiles/main.dir/object_detect.cpp.o"
+  "CMakeFiles/main.dir/utils.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
